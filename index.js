820350de6a862ffc109c1ed73d294f941ed0d568
BoubaWade <boups12@hotmail.com>
2023-01-17 15:59:29 +0100

    Projet-Personnel V2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mainContainer = document.querySelector(".main-container");
 const nbaContainer = document.querySelector(".nba-container");
 const proAContainer = document.querySelector(".pro-a-container");
 const headerContainerH1 = document.querySelector(".header-container > h1");
-// const mouseMove = document.getElementById("mousemove");
+
 const arrowRight1 = document.querySelector(".arrow-right-1");
 const arrowRight2 = document.querySelector(".arrow-right-2");
 const arrowLeft1 = document.querySelector(".arrow-left-1");
@@ -13,27 +13,6 @@ const arrayContainer = [mainContainer, nbaContainer, proAContainer];
 window.addEventListener("load", () => {
   navList.style.transform = "translateX(0)";
 });
-
-// window.addEventListener("scroll", () => {
-//   console.log(window.scrollY);
-//   if (window.scrollY > 200) {
-//     mainContainer.style.transform = "translateX(0)";
-//   }
-// });
-// } else {
-//   mainContainer.style.transform = "translateX(-750px)";
-// }
-//     if ((window.scrollY > 900) & (window.scrollY < 1300)) {
-//         nbaContainer.style.opacity = "1";
-//     } else {
-//         nbaContainer.style.opacity = "0";
-//     }
-// });
-// window.addEventListener("mousemove", (e) => {
-//   mouseMove.style.left = e.pageX + "px";
-//   mouseMove.style.top = e.pageY + "px";
-//   console.log(e);
-// });
 
 arrowRight1.addEventListener("click", () => {
   mainContainer.style.transform = "translateX(-1500px)";
